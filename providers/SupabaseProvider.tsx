332d78c6da26daba348fbc29b7446d67d429a599
rentonhead <hasancemilacar@gmail.com>
2023-08-04 15:45:04 +0300

    supabase database connect -rentboy

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useState } from "react";
+
+import { Database } from "@/types.db";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
+  const [supaBaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
+  return (
+    <SessionContextProvider supabaseClient={supaBaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+};
+
+export default SupabaseProvider;
